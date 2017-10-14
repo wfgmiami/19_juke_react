55bdb85d25c5b0e3e37bc30675caca00336f8cfc
wfgmiami <wfgmiami@yahoo.com>
2017-10-14 10:43:57 -0400

    first

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  entry: './browser/react/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
